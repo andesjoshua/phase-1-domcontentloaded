6d21c5b44e0035225c3ac16311fca900470cc1ab
Joshua Andes <andesjoshua@comcast.net>
2021-09-24 19:40:45 -0400

    commit tree 89682aee948a4f93ae195f5130429dc225612295

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", function() {
+    let text = document.getElementById('text');
+    text.textContent = 'This is really cool!';
+    // text.innerHTML = 'JavaScript is so cool. It lets me add text to my page programmatically.';
+  });
